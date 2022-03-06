9f955ccfc12272241a4dfc0c5e9eee3896652371
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2022-03-06 21:02:42 +0600

    Create medianFilter.java

---
+++ preprocessing/medianFilter.java
@@ -0,0 +1,47 @@
+package Preprocessor;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class medianFilter {
+
+    public void toMedianFilter(BufferedImage img) throws IOException {
+
+        File f=new File("photo.jpg");                               //Input Photo File
+        Color[] pixel=new Color[9];
+        int[] R=new int[9];
+        int[] B=new int[9];
+        int[] G=new int[9];
+
+        for(int i=1;i<img.getWidth()-1;i++)
+            for(int j=1;j<img.getHeight()-1;j++)
+            {
+                pixel[0]=new Color(img.getRGB(i-1,j-1));
+                pixel[1]=new Color(img.getRGB(i-1,j));
+                pixel[2]=new Color(img.getRGB(i-1,j+1));
+                pixel[3]=new Color(img.getRGB(i,j+1));
+                pixel[4]=new Color(img.getRGB(i+1,j+1));
+                pixel[5]=new Color(img.getRGB(i+1,j));
+                pixel[6]=new Color(img.getRGB(i+1,j-1));
+                pixel[7]=new Color(img.getRGB(i,j-1));
+                pixel[8]=new Color(img.getRGB(i,j));
+                for(int k=0;k<9;k++){
+                    R[k]=pixel[k].getRed();
+                    B[k]=pixel[k].getBlue();
+                    G[k]=pixel[k].getGreen();
+                }
+                Arrays.sort(R);
+                Arrays.sort(G);
+                Arrays.sort(B);
+                img.setRGB(i,j,new Color(R[4],B[4],G[4]).getRGB());
+            }
+
+        File outputfile = new File("medianFiltered.jpg");
+        ImageIO.write(img, "jpg", outputfile);
+    }
+}
+
