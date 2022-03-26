8190551947dc491e7baded5cef1b2e17059fbeaa
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2022-03-26 12:36:39 +0600

    Add files via upload

---
+++ database_connect/loginAuth/src/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.javafxloginformjdbctutorial;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+
+}
