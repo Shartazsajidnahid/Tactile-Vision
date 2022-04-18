625d15337276ae77093251af89c73b9f6e0de346
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2022-04-18 21:17:33 +0600

    Add files via upload
    Admin can add different types of users

--- database_connect/loginAuth/src/java/Authentication/AddUser.java
+++ database_connect/loginAuth/src/java/Authentication/AddUser.java
@@ -15,35 +15,49 @@ public class AddUser {
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/tactilevision";
     private static final String DATABASE_USERNAME = "root";
     private static final String DATABASE_PASSWORD = "Kakashi";
-    private static final String INSERT_QUERY = "INSERT INTO students(studentID,name,email,password) VALUES (?, ?, ?,?)";
+    private static final String INSERT_QUERY_OTHERS_1 = "INSERT INTO ";
+    private static final String INSERT_QUERY_OTHERS_2 = "(ID,fullname,password,email,phone) VALUES (?, ?, ?,?,?)";
+    private static final String INSERT_QUERY_STUDENT= "INSERT INTO student(ID,fullname,password,email,phone,roll,batch) VALUES (?, ?, ?,?,?,?,?)";
     private static final String RETRIEVE_QUERY = "select * from students";
     private static final String SELECT_QUERY = "SELECT * FROM students WHERE name = ? and password = ?";
 
-    public void addStudent(){
-
+    public void addStudent(String name, String password, String email, String phone, String roll, String batch) throws SQLException {
+        insertRecord(name,email,password,phone,roll,batch,"student");
     }
-    public void addUser(){
-
+    public void addOtherUsers(String name, String password, String email, String phone, String type) throws SQLException {
+        insertRecord(name,email,password,phone,"","",type);
     }
 
-    public void insertRecord(String fullName, String emailId, String password) throws SQLException {
+    public void insertRecord(String fullName, String emailId, String password,String phone,String roll, String batch,String type) throws SQLException {
         // Step 1: Establishing a Connection and
         // try-with-resource statement will auto close the connection.
+        String INSERT_QUERY;
+        if(type=="student"){
+            INSERT_QUERY = INSERT_QUERY_STUDENT;
+        }
+        else{
+            INSERT_QUERY= INSERT_QUERY_OTHERS_1 + type + INSERT_QUERY_OTHERS_2;
+        }
         try (Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
-
-             // Step 2:Create a statement using connection object
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY)) {
-            preparedStatement.setInt(1, 11);
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY)) {
+            preparedStatement.setString(1, "1");
             preparedStatement.setString(2, fullName);
-            preparedStatement.setString(3, emailId);
-            preparedStatement.setString(4, password);
+            preparedStatement.setString(3, password);
+            preparedStatement.setString(4, emailId);
+            preparedStatement.setString(5, phone);
 
-            System.out.println(preparedStatement);
-            // Step 3: Execute the query or update query
+            if(type == "student"){
+                preparedStatement.setString(6,roll);
+                preparedStatement.setString(7,batch);
+            }
+
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             // print SQL exception information
             this.dbutils.printSQLException(e);
         }
     }
+
+
+
 }
